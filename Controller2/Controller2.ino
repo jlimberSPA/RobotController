61bf817d0843fff1fbf14ae921736ed109bac8f3
Dad <jmlmail53@gmail.com>
2023-03-05 12:13:07 -0500

    Rebuilt Controller in Controller2 Folder

---
+++ Controller2/Controller2.ino
@@ -0,0 +1,34 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Controller2.ino
+    Created:	3/4/2023 2:05:33 PM
+    Author:     JOE-HP-LAPTOP\jmlma
+*/
+
+#include "Controller.h"
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+using namespace controller_namespace;
+controller_namespace::Robot_Controller myController = controller_namespace::Robot_Controller();
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+    myController.ControllerSetup();
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+    myController.ControllerLoop();
+
+}
